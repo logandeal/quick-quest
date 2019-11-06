41c35d6d5327efb2a1546a79f24c756f2dca7e4b
Logan Deal <53441579+logandeal@users.noreply.github.com>
2019-11-05 22:20:31 -0600

    Checkboxes

--- src/app/EventPicker.js
+++ src/app/EventPicker.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function EventPicker({ events }) {
+  const [pickedEvents, setPickedEvents] = useState([]);
+
+  function onChange(name, isChecked) {
+    const pickedEventsWithoutEvent = pickedEvents.filter(
+      event => event.name !== name
+    );
+    if (!isChecked) {
+      return setPickedEvents(pickedEventsWithoutEvent);
+    }
+    return setPickedEvents(
+      pickedEventsWithoutEvent.concat(events.find(event => event.name === name))
+    );
+  }
+
+  function isChecked(name) {
+    return pickedEvents.find(event => event.name === name) || false;
+  }
+
   return (
-    <ul>
-      {events.map(event => (
-        <li>{event.name}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {events.map(event => (
+          <li>
+            <input
+              type="checkbox"
+              id={event.name}
+              onChange={({ target }) => onChange(event.name, target.checked)}
+              checked={isChecked(event.name)}
+            />
+            <label for="{event.name}">{event.name}</label>
+          </li>
+        ))}
+      </ul>
+      Picked: {pickedEvents.map(event => event.name).join(", ")}
+    </div>
   );
 }
